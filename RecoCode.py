1788b1f05881ba2f31983ee604459864cedc2cb1
Alessandro Ruggeri <spaziale.ruggeri@hotmail.com>
2022-01-20 16:52:40 +0100

    Restructured plotting and saving and generalized event management

--- RecoCode.py
+++ RecoCode.py
@@ -15,21 +15,22 @@ import pickle
 import numpy as np
 import math as mt
 import os
+import  tkinter as tk
 #from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 #import cdist form scipy to efficiently compute distances between all pairs of points
 from scipy.spatial.distance import cdist
+
+from tkinter import filedialog
 #%%Functions for frequently used operations
 
 #computing the weight vector
-#We take the starting point and the N closest for which to compute the weights
+#We take the starting point and the closest points for which to compute the weights
 def weight(u,hits,amplitudes,h):
     w = np.zeros(hits.shape[0])
     for i in range(hits.shape[0]):
-    #placeholder weight formula
+    #computing the weights
         w[i] = (amplitudes[i]/((h**3)*((2*mt.pi)**(3/2))))*mt.exp((-1/(2*(h**2)))*np.dot((hits[i]-u),(hits[i]-u)))
-        #w[i]=amplitudes[i]
-        #print("weight {0} = {1}".format(i,w[i]))
     return w
 
 #computing the local mean
@@ -62,23 +63,6 @@ def loc_cov(u,closest,m,amplitudes,h):
     sigma = sigma/np.sum(weights)
     return sigma
 
-# #Computing the mean shift (see if it can be incorporated in another function)
-# def mean_shift(u,closest):
-#     lm=loc_mean(u,closest)-u
-#     return lm
-
-#Computing the set of N closest points
-# def N_closest(u,N):
-#     dist = np.zeros(X.shape[0])
-# #computing the distance between u and each X[i]
-#     for i in range(X.shape[0]):
-#         dist[i]=np.linalg.norm(X[i]-u)
-# #sorting the distance array 
-#     dist = np.argsort(dist,axis=0)
-# #selecting the closest N (except for u itself)
-#     dist = dist[1:N+1]
-# #slicing the X matrix over the closest indices
-#     return np.take(X,dist,axis=0), np.take(cut_array,dist,axis=0);
 
 
 #alternative: get points within N voxels
@@ -101,10 +85,12 @@ def remove_old(points,points_old,amplitudes):
 
 #cycle over the hit clusters to find possible tracks
 def track_cycle(hits, amps):
-    #set the starting point as the c.o.m. of the hits
+    #keep the complete set of voxels to compute the lpc on
+    og_hits=hits
+    og_amps=amps
     for i in range(2):
         #plotting the cut data as a heatmap
-        plot_heatmap(hits,amps)   
+        plot_heatmap(hits,amps,i)   
         print("Hits shape: ",hits.shape)
         print("Amps shape: ",amps.shape)
         try:
@@ -121,16 +107,16 @@ def track_cycle(hits, amps):
         print("-- Track {0} --".format(i+1))
         print("Start = ",start*norm)
         #perform an lpc cycle and save the resulting points (and graphs)
-        lpc_points=lpc_cycle(start,hits,amps,3)
+        lpc_points=lpc_cycle(start,og_hits,og_amps,3,i)
         print("lpc shape: ",lpc_points.shape)
         #remove from X the hits in the vicinity of the lpc curve 
-        amps=amps[np.all(cdist(hits,lpc_points)>=3/norm,axis=1)]
-        hits= hits[np.all(cdist(hits,lpc_points)>=3/norm,axis=1)]
+        amps=amps[np.all(cdist(hits,lpc_points)>=5/norm,axis=1)]
+        hits= hits[np.all(cdist(hits,lpc_points)>=5/norm,axis=1)]
 
 
 
 #Perform the LPC cycle
-def lpc_cycle(m0,hits,amps,N):
+def lpc_cycle(m0,hits,amps,N,cycle):
     pathl=0.0
     gamma_old=np.zeros(3,)
     pathl_old=0.
@@ -159,12 +145,9 @@ def lpc_cycle(m0,hits,amps,N):
         closest, amplitudes=remove_old(closest,closest_old,amplitudes)
         #compute the local mean
         m_vec[l]=loc_mean(lpc_points[l],closest,amplitudes,h)
-        #print("local mean = ",m_vec[l])
         #compute the path length
         if l>0:
             pathl+=np.linalg.norm(m_vec[l]-m_vec[l-1])
-            #print("old mean = ",m_vec[l-1])
-        #print("path length = ",pathl)
         sigma = loc_cov(lpc_points[l],closest,m_vec[l],amplitudes,h)
         try:
             val,gamma=np.linalg.eigh(sigma)
@@ -181,7 +164,6 @@ def lpc_cycle(m0,hits,amps,N):
             a=abs(np.dot(gamma,gamma_old))**2
             gamma=a*gamma+(1-a)*gamma_old
             angles[l]=1- abs(np.dot(gamma,gamma_old))
-        #print("gamma = ",gamma)
         #find the next local neighborhood point
         try:
             lpc_points[l+1]=m_vec[l]+f_b*t*gamma
@@ -232,11 +214,83 @@ def lpc_cycle(m0,hits,amps,N):
                     c=1
                     count=0
     #Draw the LPC points plot
-    save_results(N_p,h,t,lpc_points,m_vec,angles)
+    draw_plots(lpc_points,m_vec,angles,cycle)
     
     return lpc_points
     
-def save_results(N_p,h,t,lpc_points,m_vec,angles):
+def draw_plots(lpc_points,m_vec,angles,cycle):
+    #plot the data
+    #if points are to be saved check if the folder already exists
+    lpc_cache=lpc_points
+    #eliminate the unused points
+    lpc_points=lpc_points[~np.all(lpc_points==0, axis=1)]
+    lpc_points=lpc_points*norm
+    fig1 = plt.figure(figsize=(10, 10))
+    ax = fig1.add_subplot(111, projection='3d')
+    ax.set_xlim((x[0],x[x.shape[0]-1]))
+    ax.set_ylim((y[0],y[y.shape[0]-1]))
+    ax.set_zlim((z[0],z[z.shape[0]-1]))
+    img=ax.scatter(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],c=np.arange(0,lpc_points.shape[0]),s=20,marker='s')
+    plt.colorbar(img,fraction=0.025, pad=0.07,label="Point index")
+    plt.title("LPC points plot")
+    #plt.savefig('{0}/LPC_points_{1}.png'.format(folder,cycle))
+    #plt.show()
+    #Draw the LPC means plot
+    m_vec=m_vec[~np.all(m_vec==0, axis=1)]
+    m_vec=m_vec*norm
+    fig2 = plt.figure(figsize=(10, 10))
+    ax = fig2.add_subplot(111, projection='3d')
+    ax.set_xlim((x[0],x[x.shape[0]-1]))
+    ax.set_ylim((y[0],y[y.shape[0]-1]))
+    ax.set_zlim((z[0],z[z.shape[0]-1]))
+    img=ax.scatter(m_vec[:,0],m_vec[:,1],m_vec[:,2],c=np.arange(0,m_vec.shape[0]),s=20,marker='s')
+    plt.colorbar(img,fraction=0.025, pad=0.07,label="Point index")
+    plt.title("Mean points plot")
+    #plt.show()
+    #Draw the plot of eigenvector angles
+    fig3 = plt.figure(figsize=(10, 10))
+    lpc_range= np.arange(angles.shape[0])
+    plt.plot(lpc_range, angles,marker='o')
+    plt.title("Feature points plot")
+    #plt.savefig('{0}/Feature_points.png'.format(folder,cycle))
+    #plt.show()
+    #Draw the heatmap of eigenvector angles
+    angles=angles[~np.all(lpc_cache==0,axis=1)]
+    fig4 = plt.figure(figsize=(10, 10))
+    ax = fig4.add_subplot(111, projection='3d')
+    ax.set_xlim((x[0],x[x.shape[0]-1]))
+    ax.set_ylim((y[0],y[y.shape[0]-1]))
+    ax.set_zlim((z[0],z[z.shape[0]-1]))
+    img=ax.scatter(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],c=angles,s=20,marker='s')
+    plt.colorbar(img,fraction=0.025, pad=0.07,label="Angle")
+    plt.title("Eigenvector angles plot")
+    #plt.savefig('{0}/Angles_plot.png'.format(folder,cycle))
+    if question:
+        try:
+            os.mkdir(folder)
+        except FileExistsError:
+            pass
+        fig1.savefig('{0}/LPC_points_{1}.png'.format(folder,cycle))
+        fig2.savefig('{0}/Mean_points_{1}.png'.format(folder,cycle))
+        fig3.savefig('{0}/Feature_points_{1}.png'.format(folder,cycle))
+        fig4.savefig('{0}/Angles_plot_{1}.png'.format(folder,cycle))
+    plt.show()
+    # #draw the vector plot
+    # lpc_arrows=np.zeros_like(lpc_points)
+    # for i in range(1,lpc_points.shape[0]):
+    #     lpc_arrows[i]=lpc_points[i]-lpc_points[l-1]
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.set_xlim((x[0],x[x.shape[0]-1]))
+    # ax.set_ylim((y[0],y[y.shape[0]-1]))
+    # ax.set_zlim((z[0],z[z.shape[0]-1]))
+    # ax.quiver(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],lpc_arrows[:,0],lpc_arrows[:,1],lpc_arrows[:,2])
+    # plt.title("Curve {0}".format(j))
+    # plt.show()
+
+
+
+def save_results():
     #save the event parameters
     try:
         os.mkdir(folder)
@@ -244,89 +298,29 @@ def save_results(N_p,h,t,lpc_points,m_vec,angles):
         pass
     with open('{0}/parameters.txt'.format(folder), 'w') as s:
         print("Track type: {0}".format(ttype),file=s)
+        print("b_x: {0}".format(b_x),file=s)
+        print("b_y: {0}".format(b_y),file=s)
+        print("b_z: {0}".format(b_z),file=s)
         print("Track seed: {0}".format(track_seed),file=s)
         print("Cut fraction: {0}".format(cut_frac),file=s)
-        #print("Start seed: {0}".format(start_seed),file=s)
-        #print("Start: {0}".format(start*norm),file=s)
-        print("h: {0}".format(h),file=s)
-        print("t: {0}".format(t),file=s)
-        print("Number of points: {0}".format(N_p),file=s)
-        #plot the data
-        #save the lpc_points array first
-        lpc_cache=lpc_points
-        #eliminate the unused points
-        lpc_points=lpc_points[~np.all(lpc_points==0, axis=1)]
-        lpc_points=lpc_points*norm
-        fig = plt.figure(figsize=(5, 5))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim((x[0],x[x.shape[0]-1]))
-        ax.set_ylim((y[0],y[y.shape[0]-1]))
-        ax.set_zlim((z[0],z[z.shape[0]-1]))
-        img=ax.scatter(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],c=np.arange(0,lpc_points.shape[0]),s=20,marker='s')
-        plt.colorbar(img,fraction=0.025, pad=0.07)
-        plt.title("LPC points plot")
-        plt.savefig('{0}/LPC_points.png'.format(folder))
-        plt.show()
-        #Draw the LPC means plot
-        m_vec=m_vec[~np.all(m_vec==0, axis=1)]
-        m_vec=m_vec*norm
-        fig = plt.figure(figsize=(5, 5))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim((x[0],x[x.shape[0]-1]))
-        ax.set_ylim((y[0],y[y.shape[0]-1]))
-        ax.set_zlim((z[0],z[z.shape[0]-1]))
-        img=ax.scatter(m_vec[:,0],m_vec[:,1],m_vec[:,2],c=np.arange(0,m_vec.shape[0]),s=20,marker='s')
-        plt.colorbar(img,fraction=0.025, pad=0.07)
-        plt.title("Mean points plot")
-        plt.savefig('{0}/LPC_means.png'.format(folder))
-        plt.show()
-        #Draw the plot of eigenvector angles
-        fig = plt.figure(figsize=(5, 5))
-        lpc_range= np.arange(angles.shape[0])
-        plt.plot(lpc_range, angles,marker='o')
-        plt.title("Feature points plot")
-        plt.savefig('{0}/Feature_points.png'.format(folder))
-        plt.show()
-        #Draw the heatmap of eigenvector angles
-        angles=angles[~np.all(lpc_cache==0,axis=1)]
-        fig = plt.figure(figsize=(5, 5))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim((x[0],x[x.shape[0]-1]))
-        ax.set_ylim((y[0],y[y.shape[0]-1]))
-        ax.set_zlim((z[0],z[z.shape[0]-1]))
-        img=ax.scatter(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],c=angles,s=20,marker='s')
-        plt.colorbar(img,fraction=0.025, pad=0.07)
-        plt.title("Eigenvector angles plot")
-        plt.savefig('{0}/Angles_plot.png'.format(folder))
-        plt.show()
-        # #draw the vector plot
-        # lpc_arrows=np.zeros_like(lpc_points)
-        # for i in range(1,lpc_points.shape[0]):
-        #     lpc_arrows[i]=lpc_points[i]-lpc_points[l-1]
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        # ax.set_xlim((x[0],x[x.shape[0]-1]))
-        # ax.set_ylim((y[0],y[y.shape[0]-1]))
-        # ax.set_zlim((z[0],z[z.shape[0]-1]))
-        # ax.quiver(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],lpc_arrows[:,0],lpc_arrows[:,1],lpc_arrows[:,2])
-        # plt.title("Curve {0}".format(j))
-        # plt.show()
-    
-def plot_heatmap(hits,amps):
-    try:
-        os.mkdir(folder)
-    except FileExistsError:
-        pass
-    fig = plt.figure(figsize=(5, 5))
+
+
+def plot_heatmap(hits,amps,cycle):
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlim((x[0],x[x.shape[0]-1]))
     ax.set_ylim((y[0],y[y.shape[0]-1]))
     ax.set_zlim((z[0],z[z.shape[0]-1]))
     hits=hits*norm
     img=ax.scatter(hits[:,0], hits[:,1], hits[:,2], c=amps,s=20,marker='s')
-    plt.colorbar(img,fraction=0.025, pad=0.07)
+    plt.colorbar(img,fraction=0.025, pad=0.07,label="Amplitude")
     plt.title("Event heatmap")
-    plt.savefig('{0}/Ev_heatmap.png'.format(folder))
+    if question:
+        try:
+            os.mkdir(folder)
+        except FileExistsError:
+            pass
+        plt.savefig('{0}/Ev_heatmap{1}.png'.format(folder,cycle))
     plt.show()
 
 #%% Test track generation
@@ -345,7 +339,7 @@ def gaussian_test():
                 array[a][y_cor][z_cor]=amp
     return "Gaussian track", seed
 def vert_gauss_test():
-    array[:,:,:]=0.
+    array=np.zeros((50,50,130))
     seed=np.zeros(2)
     seed[0]=1392
     np.random.seed(int(seed[0]))
@@ -366,7 +360,7 @@ def vert_gauss_test():
                     z_cor=int(np.abs(np.random.normal(77.,0.5)))
                     amp=np.abs(np.random.normal(1.,0.05))
                     array[a][y_cor][z_cor]=amp
-    return "Gaussian tracks vertex",seed
+    return array,"Gaussian tracks vertex",seed
 #Test the algorithm with a made up straight line track
 #28-127-460-461-480-481 converge
 def linear_test():
@@ -399,28 +393,36 @@ def circular_test():
         y_cor= int(np.sqrt((array.shape[0]-1)**2-a**2))
         amp=np.abs(np.random.normal(1.,0.05))
         array[a][y_cor][77]=amp
-    return "Crircular arch track", seed
+    return "Circular arch track", seed
 #%%Main function
 if  __name__ == "__main__":
 #define the folder in which to save the results
-    #folder = input("Enter the folder name: ")
-    folder="default"
+    folder = input("Enter the save folder name: ") or "default"
     folder = "./Plots/{0}".format(folder)
 #Opening of the pickled file
-    dictionary=pickle.load(open("Pickles/3dreco0.pkl","rb"))
+    print("++++++++++++")
+    root = tk.Tk()
+    root.withdraw()
+    print("Select the .pkl")
+    file_sel = filedialog.askopenfilename()
+    dictionary=pickle.load(open(file_sel,"rb"))
 #We separate the UUID run key from the voxel dictionary
     key_name, dictionary = next(iter(dictionary.items()))
 #take one of the events (this will have to remain )
-#event 13 of config0 seems good
-    j=2
+    j=0
     event= dictionary[j]['amplitude']
 #convert the event dic. to numpy array
-    array=np.array(event)
-#cutting the 8 voxels in x-y closest to the masks
-#this allows to eliminate artefacts
-    array=array[:,:,7:(array.shape[2]-7)]
-#Test the algorithm with made-up tracks
-    ttype,track_seed = vert_gauss_test()
+    og_array=np.array(event)
+    ttype="Simulated event"
+    track_seed="None"
+    #Test the algorithm with made-up tracks
+    og_array,ttype,track_seed = vert_gauss_test()
+    array=np.empty_like(og_array)
+#cutting the voxels closest to the masks to avoid artifacts
+    b_x=1
+    b_y=1
+    b_z=7
+    array=og_array[b_x:(array.shape[0]-b_x),b_y:(array.shape[1]-b_y),b_z:(array.shape[2]-b_z)]
 #define the complete coordinate arrays
     x= np.arange(0.,array.shape[0],1)
     y=np.arange(0.,array.shape[1],1)
@@ -429,7 +431,7 @@ if  __name__ == "__main__":
     diff=np.max(array)-np.min(array)
     array = array/diff
 #setting the amplitude threshold to be considered
-    cut_frac=0.6
+    cut_frac=0.5
     cut=cut_frac*np.max(array)
 #performing the cut over the amplitude
     cut_array=array[array>=cut]
@@ -438,9 +440,9 @@ if  __name__ == "__main__":
     z_cut=np.nonzero(array>=cut)[2]
 #defining the matrix of cut coordinates
 #reshaping the index vectors to get the right shape for X
-    x_cut=x_cut.transpose()
-    y_cut=y_cut.transpose()
-    z_cut=z_cut.transpose()
+    x_cut=x_cut.transpose()+0.5
+    y_cut=y_cut.transpose()+0.5
+    z_cut=z_cut.transpose()+0.5
     cut_array=cut_array.transpose()
 #rows for the events, columns for the coordinates
     X=np.column_stack((x_cut,y_cut,z_cut))
@@ -450,20 +452,11 @@ if  __name__ == "__main__":
         d[i]=np.linalg.norm(X[i])
     norm=(d.max()-d.min())
     X = X/norm
-#plotting the cut data as a heatmap
-    plot_heatmap(X,cut_array)   
-#the starting point is chosen at random among the points above cut
-    # start_seed=25
-    # np.random.seed(int(start_seed))
-    # start=X[np.random.randint(0,X.shape[0]-1)]
-    # #start=np.array([7,20,78])/norm
-    # print("Start = ",start)
-    # lpc_cycle(start,3)
+    
+    question = True  
 #starting the track finding cycle
-   
     track_cycle(X,cut_array)
-    
-    
+    save_results()
     
     #array delle medie come risultato
     #Ampiezze dsi probabilità come luminosità
