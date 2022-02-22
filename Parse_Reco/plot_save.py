7b5491715d73e4f21f70ecba8e89e7332678cb0c
Alessandro Ruggeri <spaziale.ruggeri@hotmail.com>
2022-02-22 14:27:24 +0100

    Removed unused draw_plots function.

--- Parse_Reco/plot_save.py
+++ Parse_Reco/plot_save.py
@@ -10,50 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 #%%Drawing functions
-def draw_plots(lpc_points,m_vec,angles,cycle,norm,x,y,z,event_num,save_fol):
-    #plot the data
-    # lpc_cache=lpc_points
-    #eliminate the unused points
-    lpc_points=lpc_points[~np.all(lpc_points==0, axis=1)]
-    lpc_points=lpc_points*norm
-    fig1 = plt.figure(figsize=(7, 7))
-    ax = fig1.add_subplot(111, projection='3d')
-    fig1.tight_layout()
-    ax.set_xlim((x[0],x[x.shape[0]-1]))
-    ax.set_ylim((y[0],y[y.shape[0]-1]))
-    ax.set_zlim((z[0],z[z.shape[0]-1]))
-    ax.set_xlabel('x',fontsize=14,weight='bold')
-    ax.set_ylabel('y',fontsize=14,weight='bold')
-    ax.set_zlabel('z',fontsize=14,weight='bold')
-    img=ax.scatter(lpc_points[:,0],lpc_points[:,1],lpc_points[:,2],
-                   c=np.arange(0,lpc_points.shape[0]),s=20,marker='s')
-    cb=plt.colorbar(img,shrink=0.5,orientation='vertical', pad=0.1)
-    cb.set_label(label='Point index',size=14,weight='bold',labelpad=10.)
-    plt.title("Event {0} - {1} LPC points plot".format(event_num,cycle),size=20)
-    plt.savefig('{0}/Ev_{1}_LPC_c{2}.pdf'.format(save_fol,event_num,cycle))
-    plt.pause(0.05)
-    #plt.savefig('{0}/LPC_points_{1}.png'.format(folder,cycle))
-    #plt.show()
-    #Draw the LPC means plot
-    m_vec=m_vec[~np.all(m_vec==0, axis=1)]
-    m_vec=m_vec*norm
-    fig2 = plt.figure(figsize=(7,7))
-    ax = fig2.add_subplot(111, projection='3d')
-    fig2.tight_layout()
-    ax.set_xlim((x[0],x[x.shape[0]-1]))
-    ax.set_ylim((y[0],y[y.shape[0]-1]))
-    ax.set_zlim((z[0],z[z.shape[0]-1]))
-    ax.set_xlabel('x',fontsize=14,weight='bold')
-    ax.set_ylabel('y',fontsize=14,weight='bold')
-    ax.set_zlabel('z',fontsize=14,weight='bold')
-    img=ax.scatter(m_vec[:,0],m_vec[:,1],m_vec[:,2],c=np.arange(0,m_vec.shape[0]),s=20,marker='s')
-    cb=plt.colorbar(img,shrink=0.5,orientation='vertical', pad=0.1)
-    cb.set_label(label='Point index',size=14,weight='bold',labelpad=10.)
-    plt.title("Event {0} - {1} mean points plot".format(event_num,cycle),size=20)
-    plt.savefig('{0}/Ev_{1}_mean_c{2}.pdf'.format(save_fol,event_num,cycle))
-    plt.draw()
-
-
 
 def show_plot(X,cut_array,norm,x,y,z,event_num,save_fol):
     hits=X
